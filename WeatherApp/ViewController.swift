3c14a12333a0347f032e737a5164774dfcbadcca
Наталия Семичева <natalliasemicheva@ya.ru>
2018-08-11 10:27:04 +0300

    Initial commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Наталия Семичева on 8/11/18.
+//  Copyright © 2018 Наталия Семичева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
